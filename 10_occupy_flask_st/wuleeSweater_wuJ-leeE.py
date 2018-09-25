4bbcc6c75a45e4c1cd4ea62ded4d62a7194b4fea
Jeffrey Wu <jefffreywu@gmail.com>
2018-09-24 23:29:05 -0400

    updated comments

--- 10_occupy_flask_st/wuleeSweater_wuJ-leeE.py
+++ 10_occupy_flask_st/wuleeSweater_wuJ-leeE.py
@@ -15,20 +15,13 @@ occ={}
 def first(word):
     place=0
     final=0
-    count=0
     for char in word:
-        if char=='"':
-            count+=1
-        if count!=1:
-            if char==",":
-                final=place
-            else:
-                place+=1
-        else:
-            place+=1
+        if char==",":
+            final=place #finds the position of the last comma
+        place+=1
     return final
 
-file=open("occupations.csv","r")
+file=open("occupations.csv","r") #reads the csv
 
 def isnum(number):
     try:
@@ -39,19 +32,19 @@ def isnum(number):
 
 #enumerates dictionary
 for line in file:
-    place=first(line)
-    if(isnum(line[place+1:len(line)-1])):
-        occ[line[0:place]]=float(line[place+1:len(line)-1])
+    place=first(line) #finds the comma/ where to split the line
+    if(isnum(line[place+1:len(line)-1])): #filters if the line has a percentage (therefore is an occupation)
+        occ[line[0:place]]=float(line[place+1:len(line)-1]) #splits the line into the "key" before the comma and the "value" afterwards
         
-full=float(occ['Total'])
-occ.pop('Total')
+full=float(occ['Total']) #the total percentage
+occ.pop('Total') #takes out the bottom line of the total from the dictionary
 
 def randOcc():
-    ran = random.uniform(1,full)
+    ran = random.uniform(1,full) #gives a random number within a range
     count=0
     for key in occ:
-        count+=occ[key]
-        if(count>=ran):
+        count+=occ[key] #adds percentages to get rid of overlap between percentages 
+        if(count>=ran): #if the percentage is within the occupation's percentage range
             return key
 
 
@@ -66,9 +59,9 @@ def reroute(): #gives link to the occupations page
 def hello_world():
     return render_template("template.html",
                            title="Occupations",
-                           randomocc=randOcc(),
-                           first="occupation",second="percentage",
-                           diction=occ)
+                           randomocc=randOcc(), #gives random occupation
+                           first="occupation",second="percentage", #the table headings
+                           diction=occ) #the dictionary
     
 if __name__ == "__main__":
     app.debug = True
